e80a72e17e40c1046a65b91930d8f8902965fe5f
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-02-13 17:09:11 +0000

    working prototype

--- app.js
+++ app.js
@@ -40,21 +40,51 @@ else {
 //TWITTER STREAM
 
 var streamTweets = [];
-// var stream = client.stream('statuses/filter', {track: 'math'}, function(stream){
-// 	stream.on('data', function(tweet){
-// 		console.log(tweet.text);
-// 		if (streamTweets.length < 20) {
-// 			streamTweets.push(tweet.text);
+client.stream('statuses/filter', {track: 'yolo'}, function(stream){
+	client.streamServer = stream;
+	console.log('Stream running...\nTopic: yolo');
+	stream.on('data', function(tweet){
+		var tweetObj = {};
+		tweetObj.userName = tweet.user.screen_name;
+		tweetObj.text = tweet.text;
+		if (tweet.entities.urls[0]) {
+			tweetObj.expUrl = tweet.entities.urls[0].expanded_url; 
+			tweetObj.disUrl = tweet.entities.urls[0].display_url;
+		}
+		if (tweet.entities.media) {
+			tweetObj.media = tweet.entities.media[0].media_url;
+		}
+
+		if (streamTweets.length < 20) {
+			streamTweets.push(tweetObj);
+		}
+		else {
+			streamTweets.shift();
+			streamTweets.push(tweetObj);
+		}
+	});
+});
+
+// Dummy Stream
+// var num = 1;
+// setInterval(function(){
+// 	if (streamTweets.length < 20) {
+// 			count = num.toString();
+// 			streamTweets.push({userName:count,text:count,expUrl:count,disUrl:count,media:count});
+// 			console.log(streamTweets);
+// 			num++;
 // 		}
 // 		else {
+// 			count = num.toString();
 // 			streamTweets.shift();
-// 			streamTweets.push(tweet.text);
-// 		}
-// 	});
-// });
+// 			streamTweets.push({userName:count,text:count,expUrl:count,disUrl:count,media:count});
+// 			console.log(streamTweets);
+// 			num++;
+// 		}},3000);
 
 
-// TWITTER SEARCH FUNCTION  -URL (PARAM) IN - OBJECT OUT
+
+// TWITTER SEARCH FUNCTION  -URL (PARAM) IN - ARRAY OUT
 // TODO: Put in module
 function twitterSearch(query, callback) {
 	var result = [];
@@ -87,25 +117,70 @@ function twitterSearch(query, callback) {
 		}
 	});
 }
+
 // SERVER
 
 var server = http.createServer(function(req, res){
-	if (false) {
-		// if periodic stream request - send tweetStream array
+	if (req.url.match(/poll/)) {
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		res.end(JSON.stringify(streamTweets));
 	}
-	else if (false) {
-		// if filter - change stream filter HOW? put stream in function, call it?
+	else if (req.url.match(/stream/)) {
+		// TODO: Make this, and onload server, a function call
+		console.log('Stream filter change received');
+		var urlObj = url.parse(req.url,true);
+		var filter = urlObj.query.filter;
+		console.log('Filter is: '+filter);
+		if (client.streamServer) {
+			client.streamServer.destroy(); 
+		}
+		console.log('streamServer destroyed');
+		streamTweets = [];
+		// streamServer = null; => does nothing?
+		client.stream('statuses/filter', {track: filter}, function(stream){
+			client.streamServer = stream;
+			console.log('Stream running...\nTopic: '+filter);
+			stream.on('data', function(tweet){
+				var tweetObj = {};
+				tweetObj.userName = tweet.user.screen_name;
+				tweetObj.text = tweet.text;
+				if (tweet.entities.urls[0]) {
+					tweetObj.expUrl = tweet.entities.urls[0].expanded_url; 
+					tweetObj.disUrl = tweet.entities.urls[0].display_url;
+				}
+				if (tweet.entities.media) {
+					tweetObj.media = tweet.entities.media[0].media_url;
+				}
+
+				if (streamTweets.length < 20) {
+					streamTweets.push(tweetObj);
+				}
+				else {
+					streamTweets.shift();
+					streamTweets.push(tweetObj);
+				}
+			});
+		});
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		res.end(JSON.stringify(filter));
+	}
+	else if (req.url.match(/kill/)) {
+		if (client.streamServer) {
+			client.streamServer.destroy();
+			console.log('Stream server stopped'); 
+		}
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		res.end(JSON.stringify('Stream server stopped'));
 	}
 	else if (req.url.match(/search/)) {
-		// if search, call search method, return array of objects, stringified
 		console.log('Search request received');
 		var urlObj = url.parse(req.url,true);
 		var queryURL = 'search/tweets.json?q='+urlObj.query.searchterm+'&result_type=recent';
 		console.log('QueryURL: '+queryURL);
 		twitterSearch(queryURL, function(err, data){
 			if (err) {
-				res.writeHead(200, {'Content-Type': 'text/plain'});
-				res.end(err[0]);
+				res.writeHead(200, {'Content-Type': 'application/json'});
+				res.end(JSON.stringify(err[0]));
 				console.log(err[0]);
 			}
 			else {
@@ -123,4 +198,4 @@ var server = http.createServer(function(req, res){
 
 var currentPort = process.env.PORT || 8000;
 server.listen(currentPort);
-console.log('Running on port '+currentPort);
+console.log('Server running on port '+currentPort);
--- public/script.js
+++ public/script.js
@@ -1,56 +1,149 @@
 
 $(document).ready(function(){
+	var streamToggle = true;
+	var printed = [];
+	var toPrint = [];
 	
+	function getStream() {
+		$.getJSON('./poll?')
+		.done(function(tweets) {
+			console.log('tweets ',tweets);
+			console.log('printed ',printed);
+			$.each(tweets, function(ind, tweet){
+				if ($.inArray(JSON.stringify(tweet),printed) === -1) {
+					// Check if tweet is new. If so, shove in toPrint, and printed.
+					toPrint.push(tweet);
+					// track the 20 tweets on page with 'printed'
+					if(printed.length < 20) {
+						printed.push(JSON.stringify(tweet));
+					}
+					else {
+						printed.shift();
+						printed.push(JSON.stringify(tweet));
+					}
+				}
+			});
+			console.log('toPrint, before',toPrint);
+			// stick toPrint elements in DOM, then empty toPrint
+			$.each(toPrint, function(ind, tweet){ //HOW?  check for media elems, if else, append whole chunk depending on context. bleurg.
+				var urlStripped = tweet.text.replace(/(?:https?:\/\/)(?:[\w]+\.)([a-zA-Z\.]{2,63})([\/\w\.-]*)*\/?/g,'');
+				if (tweet.hasOwnProperty('media')) {
+					$('#stream-results').prepend('<div class="media stream-tweet"><div class="media-left"><a href="'+tweet.media+'"><img class="media-object" src="'+tweet.media+'"></a>'+
+												'</div><div class="media-body"><h4 class="media-heading">'+tweet.userName+'</h4><p>'+urlStripped+'</p></div></div>');
+					}
+				else if (tweet.hasOwnProperty('expUrl')) {
+				// check for twitpic etc with regex? use for src if so
+					$('#stream-results').prepend('<div class="media stream-tweet"><div class="media-left"><a href="'+tweet.expUrl+'"><img class="media-object" src="/media/link.png"></a>'+
+												'</div><div class="media-body"><h4 class="media-heading">'+tweet.userName+'</h4><p>'+urlStripped+'</p></div></div>');
+				}
+				else {
+					$('#stream-results').prepend('<div class="media stream-tweet"><div class="media-body"><h4 class="media-heading">'+tweet.userName+'</h4><p>'+urlStripped+'</p></div></div>');
+				}
+			});
+			toPrint = [];
+			console.log('toPrint, after',toPrint);
+			$('.stream-tweet').slice(20).remove();
+			if (streamToggle) {
+				setTimeout(function() {
+					getStream();
+				}, 3000);
+			}
+		});
+	}
+	// Start stream (on load)
+	if (streamToggle) {
+		setTimeout(function() {
+			getStream();
+		}, 3000);
+	}
+
 	// Search button
-	$('#search-button').click(function(){
+	$('#search-btn').click(function(){
 		var query = $('#query-field').val();
 		$('#query-field').val('');
 		$.getJSON('./search?searchterm='+query)
 		.done(function(tweets){
-			if ($('#search-results .media').length !== tweets.length) {
+			if ($.isArray(tweets)) {
+				if ($('#search-results .media').length !== tweets.length) {
+					
+					$('#search-results').empty();
+					var searchElements = '';
+					// TODO $tweets.each surely?
+					for (var i=0; i<tweets.length; i++) {
+						searchElements += '<div id="search-media-"'+i+' class="media"><div id="search-pic-'+i+'"  class="media-left"><a id="search-pic-link-'+i+'" href="#"><img id="search-pic-img-'+i+'" class="media-object" src=""></a>'+
+										'</div><div id="search-body-'+i+'" class="media-body"><h4 id="search-body-heading-'+i+'" class="media-heading">UserName</h4><p id="search-body-text-'+i+'">TweetText</p></div></div>';
+					}
+					$('#search-results').append(searchElements);
+				}
 				
-				$('#search-results').empty();
-				var searchElements = '';
-				for (var i=0; i<tweets.length; i++) {
-					searchElements += '<div id="search-pic-'+i+'" class="media"><div class="media-left"><a id="search-pic-link-'+i+'" href="#"><img id="search-pic-img-'+i+'" class="media-object" src=""></a>'+
-									'</div><div id="search-body-'+i+'" class="media-body"><h4 id="search-body-heading-'+i+'" class="media-heading">UserName</h4><p id="search-body-text-'+i+'">TweetText</p></div></div>';
-				}
-				$('#search-results').append(searchElements);
+				console.log(tweets);
+				$.each(tweets, function(ind,tweet){
+					if (tweet.hasOwnProperty('media')) {
+					$('#search-pic-link-'+ind).removeClass('hidden');
+					$('#search-pic-img-'+ind).removeClass('hidden');
+					$('#search-pic-link-'+ind).attr('href',tweet.media);
+					$('#search-pic-img-'+ind).attr('src',tweet.media);
+					}
+					else if (tweet.hasOwnProperty('expUrl')) {
+					// check for twitpic etc with regex? use for src if so
+					$('#search-pic-link-'+ind).removeClass('hidden');
+					$('#search-pic-img-'+ind).removeClass('hidden');
+					$('#search-pic-link-'+ind).attr('href',tweet.expUrl);
+					$('#search-pic-img-'+ind).attr('src','/media/link.png');
+					}
+					else {
+						$('#search-pic-link-'+ind).addClass('hidden');
+						$('#search-pic-img-'+ind).addClass('hidden');
+					}
+					console.log(tweet.userName);
+					console.log(tweet.text);
+					var urlStripped = tweet.text.replace(/(?:https?:\/\/)(?:[\w]+\.)([a-zA-Z\.]{2,63})([\/\w\.-]*)*\/?/g,'');
+					$('#search-body-heading-'+ind).text(tweet.userName);
+					$('#search-body-text-'+ind).text(urlStripped);
+
+					$('#search-results').removeClass('hidden');
+				});
 			}
-			
-			console.log(tweets);
-			$.each(tweets, function(ind,tweet){
-				if (tweet.hasOwnProperty('media')) {
-				$('#search-pic-link-'+ind).removeClass('hidden');
-				$('#search-pic-img-'+ind).removeClass('hidden');
-				$('#search-pic-link-'+ind).attr('href',tweet.media);
-				$('#search-pic-img-'+ind).attr('src',tweet.media);
-				}
-				else if (tweet.hasOwnProperty('expUrl')) {
-				// check for twitpic etc with regex? use for src if so
-				$('#search-pic-link-'+ind).removeClass('hidden');
-				$('#search-pic-img-'+ind).removeClass('hidden');
-				$('#search-pic-link-'+ind).attr('href',tweet.expUrl);
-				$('#search-pic-img-'+ind).attr('src','/media/link.png');
-				}
-				else {
-					//MAKE[ELSE] - CHECK IF NO MEDIA OR URL. HIDE MEDIA ELEM? Placeholder image?
-					$('#search-pic-link-'+ind).addClass('hidden');
-					$('#search-pic-img-'+ind).addClass('hidden');
-				}
-				// $('#search-results').on
-				console.log(tweet.userName);
-				console.log(tweet.text);
-				var urlStripped = tweet.text.replace(/(?:https?:\/\/)(?:[\w]+\.)([a-zA-Z\.]{2,63})([\/\w\.-]*)*\/?/g,'');
-				$('#search-body-heading-'+ind).text(tweet.userName);
-				$('#search-body-text-'+ind).text(urlStripped);
-			});
-			$('#search-results').removeClass('hidden');
+			// not an array? it's an error string.
+			else {
+				alert(tweets);
+			}
 		});
 	});
 
-	// Stream button. Change stream filter. Disable for n seconds.
+	// Stream button. Change stream filter. Disable for n seconds to try and avoid rate limit
+	$('#stream-filter-btn').click(function(){
+		$(this).attr('disabled','disabled');
+		streamToggle = false;
+		var query = $('#query-field').val();
+		$('#query-field').val('');
+		setTimeout(function() {$.getJSON('./stream?filter='+query)
+								.done(function(response){
+									alert('Stream filter set to: '+response);
+								});
+		},3000);
+		setTimeout(function() {streamToggle = true; getStream();},3000);
+		setTimeout(function() { $('#stream-filter-btn').removeAttr('disabled'); }, 20000); // <= bigger number?
+	});
+	// START/STOP stream display
 
-	// Start stop stream toggle button
+	$('#stream-toggle-btn').click(function(){
+		if (streamToggle) {
+			$(this).text('Start stream - local');
+			streamToggle = false;
+		}
+		else {
+			$(this).text('Stop stream - local');
+			streamToggle = true;
+			getStream();
+		}
+	});
+
+	$('#stream-kill-btn').click(function(){
+		$.getJSON('./kill')
+		.done(function(response){
+			alert('Stream killed on server. Add new filter to restart.');
+		});
+	});
 
 });
